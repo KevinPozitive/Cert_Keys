f54fa6af009a19336e2d9b82c94a09b55ad064be
Astrey <marshal.vlad.99@gmail.com>
2020-09-18 10:14:51 +0300

    Create html files

--- src/main/java/by/nces/sert_Keys/controllers/MainController.java
+++ src/main/java/by/nces/sert_Keys/controllers/MainController.java
@@ -7,9 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(Model model) {
         model.addAttribute("title", "smt");
         return "index";
     }
+    @GetMapping("/hsm")
+    public String hsm(){
+        return "hsm";
+    }
+    @GetMapping("/info")
+    public String info(){
+        return "info";
+    }
+    @GetMapping("/createKeys")
+    public String createKeys(){
+        return "createKeys";
+    }
+
 }
